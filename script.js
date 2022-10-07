8549d865fd93c8b2930cbe9ed5652a621c0857c7
Simon Gerdtman <sgerdtman@gmail.com>
2022-10-07 12:51:39 +0200

    Last Commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let domExample = document.createElement("div");
 let domExampleH2 = document.createElement("h2");
 let domExamplePara = document.createElement("p");
+let cardField = document.getElementById("cards");
 
 domExampleH2.textContent = "DOM";
 domExamplePara.textContent = "DOM eller Document Object Model går att beskriva som en hierarki utav elementen i en HTML fil. De högst upp i detta hierarki trädet är ofta taggarna <html> och <body> där diverse andra element läggs och sedna i många fall ärver och påverkas utav de attribut som ges till <html> och <body>. Inspekteras koden ser man att denna kontainer inte finns i HTML texten då den är skapad utav JavaScript som manipulerar bland annad DOM."
 domExample.classList.add("container");
 document.body.appendChild(domExample);
+cardField.appendChild(domExample);
 domExample.appendChild(domExampleH2);
 domExample.appendChild(domExamplePara);
